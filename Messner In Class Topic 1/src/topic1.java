2aa563240f02c7ad753a08b4b2620a91a64ee789
Brad D Messner <bmessner@setonhill.edu>
2020-02-05 22:55:49 -0500

    Updated Feb 5, 2020

---
+++ Messner In Class Topic 1/src/topic1.java
@@ -0,0 +1,31 @@
+//	Project:	Testing of Print and Println
+//  Author:		Brad D. Messner
+//  Date:		Feb 5, 2020
+//  Version:	1.0
+//	Git User:	bradmessner
+
+public class topic1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Declaring Variables
+		int age;
+		byte numberOfPickles = 0;
+		
+		// Modifying or updating my variables
+		age = 19;
+		age = 19 + 1;
+		age = age +7;
+		numberOfPickles = 5;
+		// Math Symbols:	+ - / * ^
+		
+		// Output content to the screen.
+		System.out.print("Hello there.\n");
+		// System.out.println("");
+		System.out.println("\tMy name is Brad.");
+		System.out.println("My age is " + age + ".");
+
+	}
+
+}
