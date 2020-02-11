b2f6545816b995140b930d29509d7de30b3a8d1d
Brad D Messner <bmessner@setonhill.edu>
2020-02-11 10:38:48 -0500

    Ready to grade.

---
+++ Messner In Class If Statements/src/inclassif.java
@@ -0,0 +1,46 @@
+//  Name:		In Class If Statements
+//  Author:		Brad D. Messner
+//  Date:		Feb 11, 2020
+//  Version:	1.0
+//	Git User:	bradmessner
+
+public class inclassif {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		// Create Variables
+		int age = 25;
+		int grade = 82;
+		
+		
+		if (age >= 21) 
+		{
+			System.out.println("You can drink whatever you want.");
+		} else 
+		{
+			System.out.println("You can have a nice glass of milk.");
+			System.out.println("Your mother said to go home.");
+		} 
+		
+		if (grade >80)
+			System.out.println("Good job.");
+		else
+		{
+			System.out.println("See me after class.");
+			System.out.println("You can do better.");
+		}
+		
+		
+		// Second Option
+		if (grade >80)
+			System.out.println("Good job.");
+		if (grade < 80)
+			System.out.println("Study a bit more.");
+		if (grade < 60)
+			System.out.println("See me after class.");
+		
+		
+	}
+
+}
